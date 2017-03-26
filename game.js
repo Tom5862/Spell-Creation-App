934210e65daa8cdb65d391e4e777cc1a6151b429
mnickels <mc.nickels2@gmail.com>
2017-03-26 10:37:06 -0700

    added keylisteners, made player movable

--- game.js
+++ game.js
@@ -5,8 +5,14 @@ var player;
  */
 function startGame() {
     player = new Player(10, 120, 24, 32, "red");
-
+    gameInput.init();
     gameScreen.init();
+}
+
+function update() {
+    player.update(gameInput.inputs);
+
+    render();
 }
 
 function render() {
@@ -24,9 +30,61 @@ var gameScreen = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.redrawTimer = setInterval(render, 20);
+        this.redrawTimer = setInterval(update, 20);
     },
     clearScreen: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+var gameInput = {
+    inputs : {
+        left : new Input([37, 65, 100]),
+        right : new Input([39, 68, 102]),
+        up : new Input([38, 87, 104]),
+        down : new Input([40, 83, 98]),
+    },
+    init : function() {
+        window.addEventListener('keydown', function(e) {
+            for (key in gameInput.inputs) {
+                if (gameInput.inputs[key].keycodes.indexOf(e.keyCode) != -1) {
+                    gameInput.inputs[key].setDown();
+                }
+            }
+            // gameInput.inputs.values().forEach(function() {
+            //     if (this.keycodes.indexOf(e.key) != -1) {
+            //         this.setDown();
+            //     }
+            // });
+        });
+        window.addEventListener('keyup', function(e) {
+            for (key in gameInput.inputs) {
+                if (gameInput.inputs[key].keycodes.indexOf(e.keyCode) != -1) {
+                    gameInput.inputs[key].setUp();
+                }
+            }
+            // gameInput.inputs.values().forEach(function() {
+            //     if (this.keycodes.indexOf(e.key) != -1) {
+            //         this.setUp();
+            //     }
+            // });
+        });
+    }
+}
+
+function Input(keycodes) {
+    this.state = false;
+    this.keycodes = keycodes;
+
+    this.isDown = function() {
+        return this.state;
+    }
+
+    this.setDown = function() {
+        this.state = true;
+    }
+
+    this.setUp = function() {
+        this.state = false;
     }
 }
--- player.js
+++ player.js
@@ -5,6 +5,26 @@ function Player(startX, startY, width, height, color) {
 	this.y = startY;
 	this.color = color;
 
+	this.update = function(keyboardInputs) {
+		console.log("update");
+		if (keyboardInputs.left.isDown()) {
+			this.x -= 2;
+			console.log("left");
+		}
+		if (keyboardInputs.right.isDown()) {
+			this.x += 2;
+			console.log("right");
+		}
+		if (keyboardInputs.up.isDown()) {
+			this.y -= 2;
+			console.log("up");
+		}
+		if (keyboardInputs.down.isDown()) {
+			this.y += 2;
+			console.log("down");
+		}
+	}
+
 	/**
 	 * render function draws this Player to the specified context
 	 * context - the context of a canvas
